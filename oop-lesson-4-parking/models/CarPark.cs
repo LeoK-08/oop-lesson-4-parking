8e8b2538a99e952a1a4d1f7ab4d7b768683f5ee3
LeoKrh <kirschl@outlook.fr>
2021-10-06 17:51:23 +0200

    Ajoutez des fichiers projet.

---
+++ oop-lesson-4-parking/models/CarPark.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lesson_4_parking
+{
+    public class CarPark
+    {
+        public List<Customer> listOfCustomers = new List<Customer>();
+        public CarParkCharge carparkCharge = new CarParkCharge();
+        public CarPark()
+        {
+            Console.WriteLine("I am the CarPark");
+        }
+        public override string ToString()
+        {
+            return "I am the CarPark()";
+        }
+        public void CalculateCharges()
+        {
+
+            foreach (Customer currentCustomer in listOfCustomers)
+            {
+                int calculatedCharge = carparkCharge.CalculateCharge(currentCustomer.hoursParked);
+                Console.WriteLine($"Calculating Charges for customer: cost is {calculatedCharge} for registration {currentCustomer.carRegistration}");
+
+            }
+
+        }
+
+    }
+}
---
+++ oop-lesson-4-parking/models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lesson_4_parking
+{
+    public class Customer
+    {
+        public int hoursParked { get; set; }
+        public string carRegistration { get; set; }
+        public Customer(string _carRegistration, int _hoursParked)
+        {
+            Console.WriteLine("I am the customer");
+            carRegistration = _carRegistration;
+            hoursParked = _hoursParked;
+        }
+        public override string ToString()
+        {
+            return "I am the customer()";
+        }
+    }
+}
